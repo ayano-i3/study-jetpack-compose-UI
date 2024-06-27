2e28396694d18722f783006b63417d2d80436dbd
ayano-i3 <ayano.o@i3-systems.com>
2024-06-27 14:23:21 +0900

    WeatherAPP 削除

--- app/src/main/java/com/example/study_jetpack_compose/WeatherApp.kt
+++ app/src/main/java/com/example/study_jetpack_compose/WeatherApp.kt
@@ -5,12 +5,9 @@ package com.example.study_jetpack_compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,22 +15,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
-//@Composable
-//fun WeatherApp(viewModel: WeatherViewModel) {
-//    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-//
-//    Scaffold(
-//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//        topBar = { WeatherTopAppBar(scrollBehavior = scrollBehavior) }
-//    ) {
-//            val weatherViewModel: WeatherViewModel = viewModel()
-//            WeatherScreen(
-//                viewModel = weatherViewModel
-//            )
-//
-//    }
-//
-//}
 @Composable
 fun WeatherScreen(viewModel: WeatherViewModel) {
     val weather by viewModel.weather.observeAsState()
@@ -57,23 +38,6 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
     }
 }
 
-@Composable
-fun WeatherTopAppBar(
-    scrollBehavior: TopAppBarScrollBehavior,
-    modifier: Modifier = Modifier
-) {
-    CenterAlignedTopAppBar(
-        scrollBehavior = scrollBehavior,
-        title = {
-            Text(
-                text = "Weather App",
-                style = MaterialTheme.typography.headlineSmall,
-            )
-        },
-        modifier = modifier
-    )
-
-}
 
 //@Preview(showBackground = true)
 //@Composable
